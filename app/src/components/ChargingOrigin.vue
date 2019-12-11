4a388d0262d12c66eab34f91f5e26f5dadf19161
m-yahya <yahya_gis@hotmail.com>
2019-12-11 17:12:42 +0100

    Add function to get unique array of objects

--- app/src/components/ChargingOrigin.vue
+++ app/src/components/ChargingOrigin.vue
@@ -501,6 +501,20 @@ export default {
       Plotly.newPlot("percentage-plot", data, layout, { responsive: true });
     },
 
+    getUnique(arr, comp) {
+      const unique = arr
+        .map(e => e[comp])
+
+        // store the keys of the unique objects
+        .map((e, i, final) => final.indexOf(e) === i && i)
+
+        // eliminate the dead keys & store unique objects
+        .filter(e => arr[e])
+        .map(e => arr[e]);
+
+      return unique;
+    },
+
     watchRealTimeProduction() {
       this.contract.events
         .Production({
